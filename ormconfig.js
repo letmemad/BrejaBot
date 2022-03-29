b103d430f3b6a651b86cf03771c68f431930ddcb
SirEliaas <sreliaas@gmail.com>
2022-03-29 17:04:25 -0300

    :fire: hotfix

--- ormconfig.js
+++ ormconfig.js
@@ -6,6 +6,7 @@ module.exports = {
   password: String(process.env.DB_PASSWORD),
   database: String(process.env.DB_DATABASE),
   entities: [String(process.env.DB_ENTITIES_PATH)],
+  migrations: [String(process.env.DB_MIGRATIONS_PATH)],
   cli: {
   entitiesDir: "src/database/entities",
   migrationsDir: "src/database/migrations",
